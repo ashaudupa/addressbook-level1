88adf9594b016aafb106d2653627bca86006349b
asha <aasha.udupa@gmail.com>
2016-08-23 14:45:20 +0800

    made changes to decodePersonsFromStrings method

--- src/seedu/addressbook/AddressBook.java
+++ src/seedu/addressbook/AddressBook.java
@@ -1177,11 +1177,11 @@ public class AddressBook {
 	 * @return if cannot decode any: empty Optional else: Optional containing
 	 *         decoded persons
 	 */
-	private static Optional<ArrayList<HashMap<String,String>>> decodePersonsFromStrings(
+	private static Optional<ArrayList<HashMap<PersonProperty,String>>> decodePersonsFromStrings(
 			ArrayList<String> encodedPersons) {
-		final ArrayList<HashMap<String,String>> decodedPersons = new ArrayList<>();
+		final ArrayList<HashMap<PersonProperty,String>> decodedPersons = new ArrayList<>();
 		for (String encodedPerson : encodedPersons) {
-			final Optional<HashMap<String, String>> decodedPerson = decodePersonFromString(encodedPerson);
+			final Optional<HashMap<PersonProperty, String>> decodedPerson = decodePersonFromString(encodedPerson);
 			if (!decodedPerson.isPresent()) {
 				return Optional.empty();
 			}
